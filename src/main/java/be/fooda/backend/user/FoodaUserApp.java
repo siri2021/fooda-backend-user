bbbbb79f5fd445ad5cba8ecf4685afb2d1c266b6
yilmazchef <yilmaz@mail.be>
2021-01-30 21:26:34 +0100

    eureka client is temporarily disabled.

--- src/main/java/be/fooda/backend/user/FoodaUserApp.java
+++ src/main/java/be/fooda/backend/user/FoodaUserApp.java
@@ -5,13 +5,11 @@ import be.fooda.backend.user.config.SwaggerConfig;
 import be.fooda.backend.user.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableSwagger2
 @Import({
         RestClientConfig.class,
