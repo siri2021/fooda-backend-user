08e321ae86c6133db738d2456804b2e8d497bdf7
yilmazchef <yilmaz@mail.be>
2021-01-31 09:40:22 +0100

    UUID binding with DB is in dev.

--- src/main/java/be/fooda/backend/user/model/entity/UserEntity.java
+++ src/main/java/be/fooda/backend/user/model/entity/UserEntity.java
@@ -20,6 +20,7 @@ public class UserEntity {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
     @EqualsAndHashCode.Include
